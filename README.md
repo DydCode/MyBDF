2735fcca96707322948a1250d919f9747ac29fa4
DydCode <70009169+DydCode@users.noreply.github.com>
2022-09-12 12:13:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-tcmpre
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-tcmpre)
---
+++ index.js
@@ -0,0 +1,10 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `
+<iframe class ='myframe' src='https://www.chrysalid.cloud/Chrysalid/Mobile/WIKI/Avvia.php?App_var[CartellaRoot]=VGVtcGxhdGUv&&hash=41DDF8357158FAA07A186AC4D90B1861'></iframe>
+
+`;
+
